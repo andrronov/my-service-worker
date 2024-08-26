0e266e4b8e5937685fe5173584499dab33775755
andrronov <109883370+andrronov@users.noreply.github.com>
2024-08-26 20:03:19 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function onWorkerReady(){
+   console.log('Worker is ready');
+}
+
+console.log('init');
+
+// navigator.serviceWorker.register('worker.js')
+navigator.serviceWorker.ready.then(onWorkerReady)
